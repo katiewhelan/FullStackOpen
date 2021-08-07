e815a28e43ce1886d852c7c4cd2108a7b08c64cb
littlewhelan <klwhelan@gmail.com>
2021-08-07 16:21:03 -0500

    exercise 2.11

--- part2b/src/App.js
+++ part2b/src/App.js
@@ -1,33 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import PersonForm from './Components/Form.js'
 import Filter from './Components/Filter'
 import DisplayList from './Components/DisplayList'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ searchList, setSearchList ] = useState(persons)
 
+  useEffect(() => {
+    console.log("Effect")
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log("Promise fulfilled", response.data)
+      setPersons(response.data)
+      console.log('after set persons ',persons)
+      setSearchList(response.data)
+    })
+  }, [])
+  console.log("render", persons.length, 'persons')
+
   const handleNameChange = (event) => {
     setNewName(event.target.value)
-
   }
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
-
   }
   const addPerson = (event) => {
-    console.log("YOU ARE HERE")
     event.preventDefault()
-    console.log('AddName Called', event)
     const personObject = {
       name : newName,
       number : newNumber
@@ -40,10 +44,8 @@ const App = () => {
   }
 
   const checkForSameName = (pO) => {
-    console.log('check same called', pO)
     const resultIsEqual = persons.some(p => isEqual(p,pO));
     if (resultIsEqual){
-      console.log('is equal')
       alert(`${newName} has already been added to the phonebook`)
     } else {
       setPersons(persons.concat(pO))
